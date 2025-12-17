9d85ccca5e2c446298039bae16a91bf48a4f9bbf
Neurosoftltdbd <neurosoftltdbd@gmail.com>
2025-12-17 19:38:29 +0600

    bug fix

--- components/AchivementSection.tsx
+++ components/AchivementSection.tsx
@@ -4,7 +4,10 @@ export default function AchivementSection() {
   return (
     <div className="w-full ">
       <div className="w-full max-w-7xl mx-auto min-h-screen">
-        <h2 className="text-2xl font-bold mb-4 text-center my-8">Our Achivements</h2>
+        <div>
+          <h2 className="text-2xl font-bold mb-4 text-center my-8">Our Achievements</h2>
+          <p></p>
+        </div>
         <div>
           
         </div>
